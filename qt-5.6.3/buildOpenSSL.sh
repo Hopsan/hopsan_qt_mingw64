cd1181daf884b6b2809de47bfe8365bcbc6b0bf9
Peter Nordin <peterNordin@users.noreply.github.com>
2017-09-12 13:12:18 +0200

    Add scripts used to build custom mingw64 Qt 5.6
    Note!
    This initial version does not build the QWebKit module since it has been removed from Qt and QWebEngine will not build with the MinGW compiler.
    QWebKit is available separately and will be added later if possible.

---
+++ qt-5.6.3/buildOpenSSL.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Change these to suit your needs
+buildroot=/c/Qt/build
+mingw64dir=/c/Qt/x86_64-4.9.3-release-posix-seh-rt_v4-rev1/mingw64
+opensslname=openssl-1.0.2l
+
+
+# Automatic code starts here
+cd $buildroot
+unset MAKE_COMMAND MAKEFLAGS
+export PATH="$mingw64dir/bin:$PATH"
+tar -xvzf $opensslname.tar.gz
+cd $opensslname
+./Configure --prefix=$buildroot/$opensslname/dist no-idea no-mdc2 no-rc5 shared mingw64
+make depend && make -j4 && make install
+echo
+echo buildOpenSSL.sh Done!
