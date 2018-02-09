c159346c9bef93c418a549d85d08b36361cf34f5
Peter Nordin <peterNordin@users.noreply.github.com>
2018-02-09 21:02:32 +0100

    Unpack source code and use correct installation

--- qt-5.6.3/buildWebKit.bat
+++ qt-5.6.3/buildWebKit.bat
@@ -13,17 +13,18 @@ set SQLITE3SRCDIR=%buildroot%\%qt_src_dir%\qtbase\src\3rdparty\sqlite
 
 set PATH=C:\GnuWin32\bin;%mingw64_path%\bin;%QTDIR%\bin;C:\Perl64\bin;C:\Python27;C:\Ruby21-x64\bin;C:\Program Files\7-Zip;%PATH%
 
+cd /D %buildroot%
+if not exist %webkit_src_dir% 7z.exe x %webkit_src_dir%.zip || echo Source directory already exist, does not unzip again, it takes to long
 cd %webkit_src_dir%
-REM set MAKE_COMMAND=mingw32-make -j6
-perl Tools\Scripts\build-webkit --qt --release --minimal --no-webkit2
 
-REM Copy (install) into Qt directory
-robocopy include %QTDIR%\include /E
-robocopy WebKitBuild\Release\lib %QTDIR%\bin *.dll
-robocopy WebKitBuild\Release\lib %QTDIR%\lib *.a
-robocopy WebKitBuild\Release\lib %QTDIR%\lib *.prl
-robocopy WebKitBuild\Release\lib\pkgconfig %QTDIR%\lib\pkgconfig /E
-robocopy WebKitBuild\Release\lib\cmake %QTDIR%\lib\cmake /E
+REM Build minimal QtWebkit
+REM set MAKE_COMMAND=mingw32-make -j6
+perl Tools\Scripts\build-webkit --qt --release --minimal --no-webkit2 --install-headers=%QTDIR%\include --install-libs=%QTDIR%\lib --qmakearg="DEFINES+=\"ENABLE_PLUGIN_PACKAGE_SIMPLE_HASH=1\"" 
+if ERRORLEVEL 1 goto end
+
+REM Install into the qt_install_directory
+perl Tools\Scripts\build-webkit --qt --release --minimal --no-webkit2 --install-headers=%QTDIR%\include --install-libs=%QTDIR%\lib --qmakearg="DEFINES+=\"ENABLE_PLUGIN_PACKAGE_SIMPLE_HASH=1\"" --makeargs="install"
+if ERRORLEVEL 1 goto end
 
 REM Package the release into a 7z archive
 cd %buildroot%
@@ -38,7 +39,7 @@ if exist %target_z7_name% (
 )
 7z a -mx7 %target_z7_name% %qt_install_dir%
 
+:end
 echo.
 echo buildWebKit.bat Done!
-REM cmd
 pause
